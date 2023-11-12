e8c92d6ec2454e50a1836d046db868497f03349f
Dominik Puz <dominikus.puz@gmail.com>
2023-11-12 12:28:33 +0100

    left sidebar

--- Twitter_Frontend/src/app/app-routing.module.ts
+++ Twitter_Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ExploreComponent} from "./components/explore/explore.component";
+import {NotificationsComponent} from "./components/notifications/notifications.component";
+import {MessagesComponent} from "./components/messages/messages.component";
+import {SettingsComponent} from "./components/settings/settings.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {TweetComponent} from "./components/tweet/tweet.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, pathMatch: 'full'},
+  {path: 'explore', component: ExploreComponent, pathMatch: 'full'},
+  {path: 'notifications', component: NotificationsComponent, pathMatch: 'full'},
+  {path: 'messages', component: MessagesComponent, pathMatch: 'full'},
+  {path: 'settings', component: SettingsComponent, pathMatch: 'full'},
+  {path: ':user-name', component: ProfileComponent, pathMatch: 'full'},
+  {path: ':user-name/:id', component: TweetComponent, pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Twitter_Frontend/src/app/app.module.ts
+++ Twitter_Frontend/src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LeftSidebarComponent} from './core/left-sidebar/left-sidebar.component';
+import {RightSidebarComponent} from './components/right-sidebar/right-sidebar.component';
+import {MainFeedComponent} from './components/main-feed/main-feed.component';
+import {HomeComponent} from './components/home/home.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { ExploreComponent } from './components/explore/explore.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    MainFeedComponent,
+    HomeComponent,
+    ExploreComponent,
+    NotificationsComponent,
+    MessagesComponent,
+    SettingsComponent,
+    ProfileComponent,
+    TweetComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
